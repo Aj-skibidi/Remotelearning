15cc53d4a413026e10c23ec3c4dd60b8afa04b60
Mathisforallhelp <1109192@lcps.org>
2024-11-22 09:30:34 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
-            return "https://play.geforcenow.com/mall/", nil
+            return "https://twoplayergames.org", nil
         },
     })
     http.ListenAndServe(":"+os.Getenv("PORT"), p)
