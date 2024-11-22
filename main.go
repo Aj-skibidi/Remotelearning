1608d3f33e460323cc739bccac8502a5f9ada3ee
Mathisforallhelp <1109192@lcps.org>
2024-11-22 08:37:19 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
-            return "https://goku.sx/", nil
+            return "https://youtube.com/", nil
         },
     })
     http.ListenAndServe(":"+os.Getenv("PORT"), p)
