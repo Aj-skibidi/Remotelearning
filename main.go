6c424272677e6fae4dd2d4f1cbd7089c676531c9
Mathisforallhelp <1109192@lcps.org>
2024-11-22 22:20:39 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
-            return "https://ww19.0123movie.net/", nil
+            return "https://parsec.com", nil
         },
     })
     http.ListenAndServe(":"+os.Getenv("PORT"), p)
